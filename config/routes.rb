5340b21971bd91a7e86684c07fe7acad04dad4cc
Ali <30610753+aamining@users.noreply.github.com>
2017-11-03 16:02:57 +1100

    order table and  orders path setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get '/orders/product/:product_id', to: 'orders#charge', as: 'order_charge' # order_charge_path
+  get '/orders/purchase', to: 'orders#purchase', as: 'order_purchase' # order_purchase_path
+  resources :orders
   resources :products
   resources :profiles
-devise_for :users
-root 'home#index'
+  devise_for :users
+  root 'home#index'
 
 
 
